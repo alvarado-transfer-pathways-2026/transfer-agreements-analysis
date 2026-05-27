9f6768bb374687ad67e26b6cede076218e718a21
Yasmin Kabir <yasminkabirofficial@gmail.com>
2026-05-26 18:04:52 -0700

    updated figures to have no title

--- question_1/scripts/scripts_for_graphs/time_to_degree_graph.py
+++ question_1/scripts/scripts_for_graphs/time_to_degree_graph.py
@@ -88,7 +88,7 @@ ax.set_ylim(0, ymax * 1.18)
 # Axis labels and title
 ax.set_ylabel("Number of Courses", fontsize=35)
 ax.set_xlabel("University of California", fontsize=35)
-plt.title("CS and Math Degree Requirements", fontsize=50)
+#plt.title("CS and Math Degree Requirements", fontsize=50)
 
 ax.set_xticks(x)
 ax.set_xticklabels([uc_display_names[uc] for uc in uc_labels], fontsize=30)
